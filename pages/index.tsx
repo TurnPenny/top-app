013c2774510348bba548e484e9e2215916f520d7
turnPenny <bohdan.miroshnik23@gmail.com>
2022-12-18 15:49:56 +0100

    Htag component created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import { Htag } from "../components";
 
 export default function Home(): JSX.Element {
-  return <div></div>;
+  return (
+    <>
+      <Htag tag='h1'>TEXT</Htag>
+    </>
+  );
 }
