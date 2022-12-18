702fd24e5e6cad10a7eb2118460953aac7a8083c
turnPenny <bohdan.miroshnik23@gmail.com>
2022-12-18 16:24:43 +0100

    Button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import { Htag } from "../components";
+import { Button, Htag } from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag='h1'>TEXT</Htag>
+      <Button>Standart</Button>
+      <Button appearance='primary'>Primary</Button>
+      <Button appearance='ghost'>ghost</Button>
     </>
   );
 }
