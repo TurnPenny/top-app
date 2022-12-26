56fed1b782ad4c6d7f23170eb70bbed0c7cacd06
turnPenny <bohdan.miroshnik23@gmail.com>
2022-12-26 19:32:37 +0100

    footer markup

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,19 @@
-import styles from "./Footer.mdoule.css";
-import { FooterProps } from "./Footer.props";
+import styles from './Footer.module.css';
+import { FooterProps } from './Footer.props';
+import cn from 'classnames';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <div className={cn(className, styles.footer)} {...props}>
+      <div>All rights reserved</div>
+      <div>
+        <a href='#' target='_blank'>
+          User agreements
+        </a>
+        <a href='#' target='_blank'>
+          User agreements
+        </a>
+      </div>
+    </div>
+  );
 };
