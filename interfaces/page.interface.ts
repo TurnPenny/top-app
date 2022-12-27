3f2f4d7042346e6c4054a3333155472e3fdcadf5
turnPenny <bohdan.miroshnik23@gmail.com>
2022-12-27 14:46:35 +0100

    added getStaticPaths

---
+++ interfaces/page.interface.ts
@@ -0,0 +1,30 @@
+export enum TopLevelCategory {
+	Courses,
+	Services,
+	Books,
+	Products
+}
+
+export interface TopPageAdvantage {
+	_id: string;
+	title: string;
+	description: string;
+}
+
+
+export interface TopPageModel {
+	tags: string[];
+	_id: string;
+	secondCategory: string;
+	alias: string;
+	title: string;
+	category: string;
+	seoText?: string;
+	tagsTitle: string;
+	metaTitle: string;
+	metaDescription: string;
+	firstCategory: TopLevelCategory;
+	advantages?: TopPageAdvantage[];
+	createdAt: Date;
+	updatedAt: Date;
+}
